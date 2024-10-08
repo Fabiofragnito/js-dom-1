786f3cc100556d87c6889adf7805ee024a6d4c00
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-08 17:39:29 +0200

    added 2nd button

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ function Lamp() {
     const button = document.querySelector('.btn');
 
     button.addEventListener('click', function () {
-        if (lampOn.style.display === 'none') {
-            lampOff.style.display = 'block';  
+        if (lampOn.style.display == 'none') {
+            lampOff.style.display = 'block'; 
+            
              
         } else {
             lampOn.style.display = 'block';   
             lampOff.style.display = 'none'; 
+            
             
         }
     });
