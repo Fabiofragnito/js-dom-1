39bfc76258bce23e2951beed92fd3e52584bee3b
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-08 15:44:01 +0200

    added event listenere on click button

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function Lamp () {
+function Lamp() {
     const lampOff = document.getElementById('lampOff');
-    const lampOn = document.getElementById('lampOn')
-    const button = document.getElementsByClassName('.btn');
-    
+    const lampOn = document.getElementById('lampOn');
+    const button = document.querySelector('.btn');
 
+    button.addEventListener('click', function () {
+        if (lampOn.style.display === 'none') {
+            lampOff.style.display = 'block';  
+             
+        } else {
+            lampOn.style.display = 'block';   
+            lampOff.style.display = 'none'; 
+            
+        }
+    });
 }
+
+// Invoco la funzione
+Lamp();
+
